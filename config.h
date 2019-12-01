5f2c45fcaf6d7d39687c18a7943a9751529d39bf
Mouad Alami <moadalami40@gmail.com>
2019-12-01 16:42:27 +0100

    Changed bg color to match i3

--- config.h
+++ config.h
@@ -8,7 +8,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char normbgcolor[]           = "#222222";
+/* static const char normbgcolor[]           = "#222222"; */
+static const char normbgcolor[]           = "#2F343F";
 static const char normbordercolor[]       = "#2f343f";
 static const char normfgcolor[]           = "#ffffff";
 static const char selfgcolor[]            = "#ffffff";
