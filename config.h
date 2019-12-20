e86b8f16a946458c04b13de3cc55e85800c45f84
Mouad Alami <moadalami40@gmail.com>
2019-12-20 20:43:48 +0100

    increased dmenu font size

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char dmenufont[]       = "monospace:size=12";
 static const char normbgcolor[]           = "#2F343F";
 static const char normbordercolor[]       = "#2f343f";
 static const char normfgcolor[]           = "#ffffff";
