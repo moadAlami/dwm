8cdb2e0b28395d80088b3f7af6c3e8cdafe1e74c
Mouad Alami <moadalami40@gmail.com>
2020-06-27 12:58:51 +0100

    removed comment

--- config.h
+++ config.h
@@ -114,7 +114,6 @@ static Key keys[] = {
 	/* modifier             key        					function        argument */
 
 	{ 0,					XK_Num_Lock,				spawn,			SHCMD("kill -9 $(cat /home/mouad/github/dwm-bar/dwmpid)") },
-	/* { 0,					XK_Num_Lock,				spawn,			SHCMD("notify-send 'numlock trigger' && pkill -RTMIN+11 dwmblocks") }, */
 	{ MODKEY|ShiftMask,		XK_o,						spawn,			SHCMD("[ $(echo -e 'Yes\nNo' | dmenu -i -p 'Are you sure you want to shutdown?') == 'Yes' ] && shutdown now && rm /home/mouad/github/dwm-bar/dwmpid") },
 	{ MODKEY|ShiftMask,		XK_x,						spawn,			SHCMD("i3lock") },
 	{ MODKEY,				XK_r,						spawn,			SHCMD("st -e ranger") },
