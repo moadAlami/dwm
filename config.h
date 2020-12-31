72e3b121caaff05a2fbd70c8426dc0bd0114625c
moadAlami <moadalami40@gmail.com>
2020-12-31 08:04:21 +0100

    mod+r launches st with lf

--- config.h
+++ config.h
@@ -115,7 +115,7 @@ static Key keys[] = {
 	{ 0,					XK_Num_Lock,				spawn,			SHCMD("kill -9 $(cat /home/mouad/github/dwm-bar/dwmpid)") },
 	{ MODKEY|ShiftMask,		XK_o,						spawn,			SHCMD("[ $(echo -e 'Yes\nNo' | dmenu -i -p 'Are you sure you want to shutdown?') == 'Yes' ] && shutdown now && rm /home/mouad/github/dwm-bar/dwmpid") },
 	{ MODKEY|ShiftMask,		XK_x,						spawn,			SHCMD("i3lock -c 252525") },
-	{ MODKEY,				XK_r,						spawn,			SHCMD("st -e ranger") },
+	{ MODKEY,				XK_r,						spawn,			SHCMD("st -e lf") },
 
 	{ MODKEY,               XK_t,						setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,               XK_space,					setlayout,      {.v = &layouts[1]} },
