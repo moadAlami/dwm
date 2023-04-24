6d5bbd5f668e4f4a238dade335b837f645a37239
Mouad Alami <moadalami40@gmail.com>
2023-04-24 23:20:21 +0100

    udpated colors

--- config.h
+++ config.h
@@ -8,16 +8,17 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 
 static const char *fonts[] = { "monospace:size=11",
     "Noto Color Emoji:pixelsize=11:antialias=true:autohint=true"};
-static const char dmenufont[]       = "monospace:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char dmenufont[]             = "monospace:size=12";
+static const char normbgcolor[]           = "#222222";
+static const char normbordercolor[]       = "#444444";
+static const char normfgcolor[]           = "#bbbbbb";
+static const char selfgcolor[]            = "#eeeeee";
+static const char selbordercolor[]        = "#005577";
+static const char selbgcolor[]            = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor } ,
+	[SchemeSel]  = { selfgcolor , selbgcolor ,  selbordercolor  },
 };
 
 #include "movestack.c"
@@ -75,7 +76,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", "#000000", "-nf", "#268bd2", "-sb", "#268bd2", "-sf", "#000000", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
