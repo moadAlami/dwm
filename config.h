7b260620e770659dec436a90ba9101d37017dc32
Mouad Alami <moadalami40@gmail.com>
2023-04-24 23:23:46 +0100

    bar height patch

--- config.h
+++ config.h
@@ -5,6 +5,7 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 2;        /* 2 is the default spacing around the bar's font */
 
 static const char *fonts[] = { "monospace:size=11",
     "Noto Color Emoji:pixelsize=11:antialias=true:autohint=true"};
