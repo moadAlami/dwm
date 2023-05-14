10a042a3b9b62100cab4360ab4edffbae0c4f07b
Mouad Alami <moadalami40@gmail.com>
2023-05-14 06:38:10 +0100

    bind some classes to tags

--- config.h
+++ config.h
@@ -45,8 +45,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",	NULL,       NULL,       1 << 5,       1,           -1 },
+	{ "discord",	NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Steam",	NULL,       NULL,       1 << 4,       0,           -1 },
 };
 
 /* layout(s) */
