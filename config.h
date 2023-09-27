8a942f660d2968785d48db66628ab44efdb1885d
moadAlami <moadalami40@gmail.com>
2023-09-27 07:00:07 +0100

    start scratchpad with tmux renamed

--- config.h
+++ config.h
@@ -85,7 +85,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", "#000000", "-nf", "#268bd2", "-sb", "#268bd2", "-sf", "#000000", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", "tmux", "new", "-s", "scratch", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
