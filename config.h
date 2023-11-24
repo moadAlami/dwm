bc4b3bc695e8fbe3dbb2cc2def48d841a68dde79
moadAlami <moadalami40@gmail.com>
2023-11-24 18:32:41 +0100

    removed floating layout

--- config.h
+++ config.h
@@ -54,7 +54,6 @@ static const Rule rules[] = {
 	{ "steam",	NULL,       NULL,       1 << 4,       0,           -1 },
 };
 
-/* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
@@ -63,7 +62,6 @@ static const int lockfullscreen = 0; /* 1 will force focus on the fullscreen win
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
@@ -105,8 +103,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_space,  setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,			XK_f,      togglefullscr,  {0} },
 	{ MODKEY|ShiftMask,             XK_t,      togglefakefullscreen, {0} },
